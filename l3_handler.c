e8cc7a1b48bd34f0c801482c6988117bdb00dd7e
Mate Soos <soos.mate@gmail.com>
2014-10-27 15:40:37 +0100

    More verbose messages

--- l3_handler.c
+++ l3_handler.c
@@ -1231,20 +1231,37 @@ void handle_radio_msg(struct session_info *s, struct radio_message *m)
 		case MSG_SACCH: //slow associated control channel
 			if (s->rat != RAT_GSM)
 				break;
+
+			if (msg_verbose) {
+				fprintf(stderr, "-> MSG_SACCH\n");
+			}
 			handle_lapdm(s, &s->chan_sacch[ul], &m->msg[2], m->msg_len-2, m->bb.fn[0], ul);
 			break;
 		case MSG_SDCCH: //standalone dedicated control channel
 			if (s->rat != RAT_GSM)
 				break;
+
+			if (msg_verbose) {
+				fprintf(stderr, "-> MSG_SDCCH\n");
+			}
 			handle_lapdm(s, &s->chan_sdcch[ul], m->msg, m->msg_len, m->bb.fn[0], ul);
 			break;
 		case MSG_FACCH:
+			if (msg_verbose) {
+				fprintf(stderr, "-> MSG_FACCH\n");
+			}
 			handle_lapdm(s, &s->chan_facch[ul], m->msg, m->msg_len, m->bb.fn[0], ul);
 			break;
 		case MSG_BCCH:
+			if (msg_verbose) {
+				fprintf(stderr, "-> MSG_BCCH\n");
+			}
 			handle_dtap(s, &m->msg[1], m->msg_len-1, m->bb.fn[0], ul);
 			break;
 		default:
+			if (msg_verbose) {
+				fprintf(stderr, "Wrong MSG flags %02x\n", m->flags);
+			}
 			printf("Wrong MSG flags %02x\n", m->flags);
 			abort();
 		}
