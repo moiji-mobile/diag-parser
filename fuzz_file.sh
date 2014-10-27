e2452ac2346d5dbecdd2eb45a562a069db0a7d72
Luca Melette <luca@srlabs.de>
2014-10-27 19:51:42 +0100

    More fuzziing in auto-fuzz

--- fuzz_file.sh
+++ fuzz_file.sh
@@ -2,8 +2,8 @@
 
 OUTFILE=${1}.fuzzed
 
-for i in `seq -w 01 1 50`; do
- dd if=$1 of=fuzz.block.${i} skip=$RANDOM bs=$((RANDOM/256)) count=$((RANDOM/256)) > /dev/null 2> /dev/null
+for i in `seq -w 01 1 99`; do
+ dd if=$1 of=fuzz.block.$RANDOM skip=$RANDOM bs=$((RANDOM/256)) count=$((RANDOM/256)) > /dev/null 2> /dev/null
 done
 
 cat fuzz.block.* > $OUTFILE
