b57523debaa26303b976da02238221e025a39bbf
Luca Melette <luca@srlabs.de>
2014-11-07 19:42:27 +0100

    Changing auto_import script to be easily used with find

--- diag_auto_import.sh
+++ diag_auto_import.sh
@@ -3,5 +3,6 @@
 NEXT_SESSION=`echo "select max(id)+1 from session_info;" | mysql -uroot celldb -N`
 NEXT_CELL=`echo "select max(id)+1 from cell_info;" | mysql -uroot celldb -N`
 
-./diag_import $NEXT_SESSION $NEXT_CELL
+echo "Importing $1"
+./diag_import $NEXT_SESSION $NEXT_CELL < $1
 #gdb diag_import
