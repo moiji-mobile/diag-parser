1c82ca130593af4638840fa7f0e98cc28b4cd3d3
Mate Soos <soos.mate@gmail.com>
2014-11-24 21:58:00 +0100

    Fixing umts_rrc to parse only messages that are needed

--- umts_rrc.c
+++ umts_rrc.c
@@ -41,33 +41,25 @@ int handle_dcch_ul(struct session_info *s, uint8_t *msg, size_t len)
 	}
 
 	/* Attach description and discard unsupported types */
+	int need_to_parse = 0;
 	switch (msg_type) {
-	case 18:
-		SET_MSG_INFO(s, "RRC ConnectionSetupComplete");
-		goto ul_no_free;
-	}
-
-	rv = uper_decode(NULL, &asn_DEF_UL_DCCH_Message, (void **) &dcch, msg, len, 0, 0);
-	if ((rv.code != RC_OK) || !dcch) {
-		SET_MSG_INFO(s, "ASN.1 PARSING ERROR");
-		return 1;
-	}
-
-	switch(dcch->message.present) {
-	case UL_DCCH_MessageType_PR_rrcConnectionSetupComplete:
+	case (UL_DCCH_MessageType_PR_rrcConnectionSetupComplete-1):
 		SET_MSG_INFO(s, "RRC Setup Complete");
 		session_reset(&s[0], 1);
 		s[1].new_msg = NULL;
 		session_reset(&s[1], 1);
 		break;
-	case UL_DCCH_MessageType_PR_rrcConnectionReleaseComplete:
+
+	case (UL_DCCH_MessageType_PR_rrcConnectionReleaseComplete-1):
 		SET_MSG_INFO(s, "RRC Release Complete");
 		session_reset(&s[0], 0);
 		s[1].new_msg = NULL;
 		session_reset(&s[1], 0);
 		break;
-	case UL_DCCH_MessageType_PR_securityModeComplete:
+
+	case (UL_DCCH_MessageType_PR_securityModeComplete-1):
 		SET_MSG_INFO(s, "RRC Security Mode Complete");
+
 		//TODO try to handle domains
 		if (s->cipher_missing < 0) {
 			s->cipher_missing = 0;
@@ -75,29 +67,59 @@ int handle_dcch_ul(struct session_info *s, uint8_t *msg, size_t len)
 			s->cipher_missing = 1;
 		}
 		break;
-	case UL_DCCH_MessageType_PR_initialDirectTransfer:
+
+	case (UL_DCCH_MessageType_PR_initialDirectTransfer-1):
 		SET_MSG_INFO(s, "RRC InitialDirectTransfer");
-		domain = dcch->message.choice.initialDirectTransfer.cn_DomainIdentity;
-		nas = dcch->message.choice.initialDirectTransfer.nas_Message.buf;
-		nas_len = dcch->message.choice.initialDirectTransfer.nas_Message.size;
+
+		need_to_parse = 1;
 		break;
-	case UL_DCCH_MessageType_PR_uplinkDirectTransfer:
+
+	case (UL_DCCH_MessageType_PR_uplinkDirectTransfer-1):
 		SET_MSG_INFO(s, "RRC UplinkDirectTransfer");
-		domain = dcch->message.choice.initialDirectTransfer.cn_DomainIdentity;
-		nas = dcch->message.choice.uplinkDirectTransfer.nas_Message.buf;
-		nas_len = dcch->message.choice.uplinkDirectTransfer.nas_Message.size;
+
+		need_to_parse = 1;
 		break;
+
 	default:
 		SET_MSG_INFO(s, "UL-DCCH type=%d", dcch->message.present);
 		s->new_msg->flags &= ~MSG_DECODED;
 	}
 
-	if (domain >= 0) {
-		s->new_msg->domain = domain;
-	}
 
-	if (nas) {
-		handle_dtap(s, nas, nas_len, 0, 1);
+	if (need_to_parse) {
+		rv = uper_decode(NULL, &asn_DEF_UL_DCCH_Message, (void **) &dcch, msg, len, 0, 0);
+		if ((rv.code != RC_OK) || !dcch) {
+			SET_MSG_INFO(s, "ASN.1 PARSING ERROR");
+			return 1;
+		}
+
+		switch(dcch->message.present) {
+		case UL_DCCH_MessageType_PR_initialDirectTransfer:
+			domain = dcch->message.choice.initialDirectTransfer.cn_DomainIdentity;
+			nas = dcch->message.choice.initialDirectTransfer.nas_Message.buf;
+			nas_len = dcch->message.choice.initialDirectTransfer.nas_Message.size;
+			break;
+
+		case UL_DCCH_MessageType_PR_uplinkDirectTransfer:
+			domain = dcch->message.choice.initialDirectTransfer.cn_DomainIdentity;
+			nas = dcch->message.choice.uplinkDirectTransfer.nas_Message.buf;
+			nas_len = dcch->message.choice.uplinkDirectTransfer.nas_Message.size;
+			break;
+
+		default:
+			assert(0 && "This message is not meant to be parsed, error!");
+			break;
+		}
+
+		if (domain >= 0) {
+			s->new_msg->domain = domain;
+		}
+
+		if (nas) {
+			handle_dtap(s, nas, nas_len, 0, 1);
+		}
+
+		ASN_STRUCT_FREE(asn_DEF_UL_DCCH_Message, dcch);
 	}
 
 #if 0
@@ -107,9 +129,6 @@ int handle_dcch_ul(struct session_info *s, uint8_t *msg, size_t len)
 		//	 dcch->integrityCheckInfo->rrc_MessageSequenceNumber);
 	}
 #endif
-
-	ASN_STRUCT_FREE(asn_DEF_UL_DCCH_Message, dcch);
-	ul_no_free:
 
 	return 0;
 }
@@ -154,83 +173,81 @@ int handle_dcch_dl(struct session_info *s, uint8_t *msg, size_t len)
 	}
 
 	/* Attach description and discard unsupported types */
+	int need_to_parse = 0;
 	switch (msg_type) {
-	case 3:
-		SET_MSG_INFO(s, "RRC CellUpdateConfirm");
-		goto dl_no_free;
-	case 8:
-		SET_MSG_INFO(s, "RRC MeasurementControl");
-		goto dl_no_free;
-	case 10:
-		SET_MSG_INFO(s, "RRC PhysicalChannelReconfig");
-		goto dl_no_free;
-	case 11:
-		SET_MSG_INFO(s, "RRC PhysicalSharedChannelAllocation");
-		goto dl_no_free;
-	case 12:
-		SET_MSG_INFO(s, "RRC RadioBearerReconfig");
-		goto dl_no_free;
-	case 13:
-		SET_MSG_INFO(s, "RRC RadioBearerRelease");
-		goto dl_no_free;
-	case 14:
-		SET_MSG_INFO(s, "RRC RadioBearerSetup");
-		goto dl_no_free;
-	case 18:
-		SET_MSG_INFO(s, "RRC TransportChannelReconfig");
-		goto dl_no_free;
-	case 24:
-		SET_MSG_INFO(s, "RRC utranMobilityInformation");
-		goto dl_no_free;
-	case 25:
-		SET_MSG_INFO(s, "RRC handoverFromUTRAN");
-		goto dl_no_free;
-	}
-
-	/* Call ASN.1 decoder */
-	rv = uper_decode(NULL, &asn_DEF_DL_DCCH_Message, (void **) &dcch, msg, len, 0, 0);
-	if ((rv.code != RC_OK) || !dcch) {
-		SET_MSG_INFO(s, "ASN.1 PARSING ERROR");
-		return 1;
-	}
-
-	/* Process contents by message type */
-	switch(dcch->message.present) {
-	case DL_DCCH_MessageType_PR_signallingConnectionRelease:
-		SET_MSG_INFO(s, "RRC Signalling Connection Release");
-		break;
-	case DL_DCCH_MessageType_PR_rrcConnectionRelease:
-		SET_MSG_INFO(s, "RRC Connection Release");
-		session_reset(&s[0], 0);
-		s[1].new_msg = NULL;
-		session_reset(&s[1], 0);
-		break;
-	case DL_DCCH_MessageType_PR_securityModeCommand:
-		switch(dcch->message.choice.securityModeCommand.present) {
-		case SecurityModeCommand_PR_r3:
-			domain = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.cn_DomainIdentity;
-			//cap = &dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.securityCapability;
-			cipher = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.cipheringModeInfo;
-			integrity = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.integrityProtectionModeInfo;
-			if (cipher && (cipher->cipheringModeCommand.present == CipheringModeCommand_PR_startRestart)) {
-				c_algo = cipher->cipheringModeCommand.choice.startRestart;
-			}
-			if (integrity && integrity->integrityProtectionAlgorithm) {
-				i_algo = 1 + *(integrity->integrityProtectionAlgorithm);
-			}
+		case (DL_DCCH_MessageType_PR_signallingConnectionRelease-1):
+			SET_MSG_INFO(s, "RRC Signalling Connection Release");
 			break;
-		case SecurityModeCommand_PR_later_than_r3:
-			switch(dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.present) {
-			case SecurityModeCommand__later_than_r3__criticalExtensions_PR_r7:
-				domain = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.cn_DomainIdentity;
-				//cap = &dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.securityCapability;
-				cipher7 = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.cipheringModeInfo;
-				integrity7 = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.integrityProtectionModeInfo;
-				if (cipher7) {
-					c_algo = cipher7->cipheringModeCommand.startRestart;
+
+		case (DL_DCCH_MessageType_PR_rrcConnectionRelease-1):
+			SET_MSG_INFO(s, "RRC Connection Release");
+			session_reset(&s[0], 0);
+			s[1].new_msg = NULL;
+			session_reset(&s[1], 0);
+			break;
+
+		case (DL_DCCH_MessageType_PR_securityModeCommand-1):
+			need_to_parse = 1;
+			break;
+
+		case (SecurityModeCommand_PR_later_than_r3-1):
+			need_to_parse = 1;
+			break;
+
+		case (DL_DCCH_MessageType_PR_downlinkDirectTransfer-1):
+			SET_MSG_INFO(s, "RRC DownlinkDirectTransfer");
+
+			need_to_parse = 1;
+			break;
+
+		default:
+			SET_MSG_INFO(s, "DL-DCCH type=%d", dcch->message.present);
+			s->new_msg->flags &= ~MSG_DECODED;
+			break;
+	}
+
+	if (need_to_parse) {
+		/* Call ASN.1 decoder */
+		rv = uper_decode(NULL, &asn_DEF_DL_DCCH_Message, (void **) &dcch, msg, len, 0, 0);
+		if ((rv.code != RC_OK) || !dcch) {
+			SET_MSG_INFO(s, "ASN.1 PARSING ERROR");
+			return 1;
+		}
+
+		/* Process contents by message type */
+		switch(dcch->message.present) {
+		case DL_DCCH_MessageType_PR_securityModeCommand:
+			switch(dcch->message.choice.securityModeCommand.present) {
+			case SecurityModeCommand_PR_r3:
+				domain = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.cn_DomainIdentity;
+				//cap = &dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.securityCapability;
+				cipher = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.cipheringModeInfo;
+				integrity = dcch->message.choice.securityModeCommand.choice.r3.securityModeCommand_r3.integrityProtectionModeInfo;
+				if (cipher && (cipher->cipheringModeCommand.present == CipheringModeCommand_PR_startRestart)) {
+					c_algo = cipher->cipheringModeCommand.choice.startRestart;
 				}
-				if (integrity7 && integrity7->integrityProtectionAlgorithm) {
-					i_algo = 1 + *(integrity7->integrityProtectionAlgorithm);
+				if (integrity && integrity->integrityProtectionAlgorithm) {
+					i_algo = 1 + *(integrity->integrityProtectionAlgorithm);
+				}
+				break;
+			case SecurityModeCommand_PR_later_than_r3:
+				switch(dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.present) {
+				case SecurityModeCommand__later_than_r3__criticalExtensions_PR_r7:
+					domain = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.cn_DomainIdentity;
+					//cap = &dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.securityCapability;
+					cipher7 = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.cipheringModeInfo;
+					integrity7 = dcch->message.choice.securityModeCommand.choice.later_than_r3.criticalExtensions.choice.r7.securityModeCommand_r7.integrityProtectionModeInfo;
+					if (cipher7) {
+						c_algo = cipher7->cipheringModeCommand.startRestart;
+					}
+					if (integrity7 && integrity7->integrityProtectionAlgorithm) {
+						i_algo = 1 + *(integrity7->integrityProtectionAlgorithm);
+					}
+					break;
+				default:
+					SET_MSG_INFO(&s[0], "RRC Security Mode Command / Not supported");
+					error = 1;
+					goto dl_end;
 				}
 				break;
 			default:
@@ -238,59 +255,55 @@ int handle_dcch_dl(struct session_info *s, uint8_t *msg, size_t len)
 				error = 1;
 				goto dl_end;
 			}
+
+			if (s[domain].cipher) {
+				printf("Transaction was already ciphered!\n");
+				error = 1;
+				goto dl_end;
+			}
+			SET_MSG_INFO(&s[domain], "RRC Security Mode Command (%s), UEA/%d UIA/%d", (domain ? "PS" : "CS"), c_algo, i_algo);
+			s[domain].cipher = c_algo;
+			s[domain].integrity = i_algo;
+			s[domain].cipher_missing = -1;
 			break;
+
+		case DL_DCCH_MessageType_PR_downlinkDirectTransfer:
+			switch(dcch->message.choice.downlinkDirectTransfer.present) {
+			case DownlinkDirectTransfer_PR_r3:
+				domain = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.cn_DomainIdentity;
+				nas = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.nas_Message.buf;
+				nas_len = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.nas_Message.size;
+				break;
+			case DownlinkDirectTransfer_PR_later_than_r3:
+				SET_MSG_INFO(s, "DDT > r3 is not supported!");
+				break;
+			default:
+				SET_MSG_INFO(s, "DDT / Not supported");
+				error = 1;
+				goto dl_end;
+			}
+			break;
+
 		default:
-			SET_MSG_INFO(&s[0], "RRC Security Mode Command / Not supported");
-			error = 1;
-			goto dl_end;
-		}
-		if (s[domain].cipher) {
-			printf("Transaction was already ciphered!\n");
-			error = 1;
-			goto dl_end;
-		}
-		SET_MSG_INFO(&s[domain], "RRC Security Mode Command (%s), UEA/%d UIA/%d", (domain ? "PS" : "CS"), c_algo, i_algo);
-		s[domain].cipher = c_algo;
-		s[domain].integrity = i_algo;
-		s[domain].cipher_missing = -1;
-		break;
-	case DL_DCCH_MessageType_PR_downlinkDirectTransfer:
-		SET_MSG_INFO(s, "RRC DownlinkDirectTransfer");
-		switch(dcch->message.choice.downlinkDirectTransfer.present) {
-		case DownlinkDirectTransfer_PR_r3:
-			domain = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.cn_DomainIdentity;
-			nas = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.nas_Message.buf;
-			nas_len = dcch->message.choice.downlinkDirectTransfer.choice.r3.downlinkDirectTransfer_r3.nas_Message.size;
+			assert(0 && "This message is not meant to be parsed, error!");
 			break;
-		case DownlinkDirectTransfer_PR_later_than_r3:
-			SET_MSG_INFO(s, "DDT > r3 is not supported!");
-			break;
-		default:
-			SET_MSG_INFO(s, "DDT / Not supported");
-			error = 1;
-			goto dl_end;
 		}
-		break;
-	default:
-		SET_MSG_INFO(s, "DL-DCCH type=%d", dcch->message.present);
-		s->new_msg->flags &= ~MSG_DECODED;
+
+		if (nas) {
+			handle_dtap(s, nas, nas_len, 0, 0);
+		}
+
+		dl_end:
+		ASN_STRUCT_FREE(asn_DEF_DL_DCCH_Message, dcch);
 	}
 
-	if (nas) {
-		handle_dtap(s, nas, nas_len, 0, 0);
-	}
-
-#if 0
+	#if 0
 	if (dcch->integrityCheckInfo) {
 		mac = &dcch->integrityCheckInfo->messageAuthenticationCode;
 		//SET_MSG_INFO(s, " [I:%s/%lu]", osmo_hexdump_nospc(mac->buf, mac->size),
 		//	 dcch->integrityCheckInfo->rrc_MessageSequenceNumber);
 	}
-#endif
-
-dl_end:
-	ASN_STRUCT_FREE(asn_DEF_DL_DCCH_Message, dcch);
-dl_no_free:
+	#endif
 
 	return error;
 }
