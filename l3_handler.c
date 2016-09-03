db3ae4b3829ff66e56b0bf5f50ced956d478b7d8
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-09-03 23:58:58 +0200

    misc: Drop umts decoding to avoid asn1c dependency
    This will lead to some more duplicates (e.g. different tracing
    layers reporting sending a GSM04.08 message) but it is something
    that we are willing to deal with.

--- l3_handler.c
+++ l3_handler.c
@@ -1447,23 +1447,10 @@ void handle_radio_msg(struct session_info *s, struct radio_message *m)
 		if (m->flags & MSG_SDCCH) {
 			s[0].rat = RAT_UMTS;
 			s[1].rat = RAT_UMTS;
-
-			if (ul) {
-				handle_dcch_ul(s, m->bb.data, m->msg_len);
-			} else {
-				handle_dcch_dl(s, m->bb.data, m->msg_len);
-			}
 		} else if (m->flags & MSG_FACCH) {
 			s[0].rat = RAT_UMTS;
 			s[1].rat = RAT_UMTS;
-
-			if (ul) {
-				handle_ccch_ul(s, m->bb.data, m->msg_len);
-			} else {
-				handle_ccch_dl(s, m->bb.data, m->msg_len);
-			}
 		} else if (m->flags & MSG_BCCH) {
-			handle_umts_bcch(s, m->bb.data, m->msg_len);
 		} else {
 			assert(0);
 		}
